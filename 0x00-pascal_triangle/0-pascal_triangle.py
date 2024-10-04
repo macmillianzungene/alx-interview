a43a86b48dfc64631c21772ad8a27757c7e71e4a
macmillianzungene <macmillianzungene@gmail.com>
2024-10-04 04:24:08 +0200

    updated

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python3
+"""
+Generate Pascal's Triangle up to the nth row.
+
+This function generates Pascal's Triangle up to the specified number of rows.
+Pascal's Triangle is a triangular array of the binomial coefficients.
+
+Args:
+    n (int): The number of rows of Pascal's Triangle to generate.
+
+Returns:
+    list: A list of lists, where each inner list represents a row of Pascal's Triangle.
+"""
+
 def pascal_triangle(n):
     if n <= 0:
         return []
